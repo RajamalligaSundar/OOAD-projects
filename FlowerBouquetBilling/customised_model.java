ae4ee970173f90ca8ddb165cfe8b78bdccf0d675
RajamalligaSundar <rajamalligasundar@gmail.com>
2022-11-03 09:28:33 +0530

    update file

--- FlowerBouquetBilling/customised_model.java
+++ FlowerBouquetBilling/customised_model.java
@@ -12,8 +12,15 @@ class customised_model{
     int daisy = 30;
     int vilot = 40;
 
+     //available templates 
+     int day_dreams = 150;
+     int fancy_floral = 250;
+     int tropical_breeze = 200;
+     int velvet_valley = 350;
+     int forest_fantasy =200;
+
     int bill = 0;
-    //pre-defined design la adding n numbers
+   
     private int CalculateBill_General_Customization()
     {
         int bill_amount = 0; 
@@ -62,10 +69,49 @@ class customised_model{
     }
 
     //selective customization
-
-    private int CalculateBill_Selective_Customization()
+    
+    private int existing_bouquests()
     {
-        int bill_amount=0;
+        int bill_amount = 0; 
+
+        System.out.println("\nList of Bouquests: \nPress, \n\t 1 for Day Dreams\n\t 2 for Fancy Floral\n\t 3 for Tropical breeze \n\t 4 for Velvet valley\n\t 5 for Forest fantancy");
+
+        int choice = sc.nextInt();
+    
+        switch(choice)
+        {
+            case 1:
+            System.out.println("You have choosen DAY DREAMS");
+            bill_amount = day_dreams;
+            break;
+            case 2:
+            System.out.println("You have choosen FANCY FLORAL");
+            bill_amount = fancy_floral;
+            break;
+            case 3:
+            System.out.println("You have choosen TROPICAL BREEZE");
+            bill_amount = tropical_breeze;
+            break;
+            case 4:
+            System.out.println("You have choosen VELVET VALLEY");
+            bill_amount = velvet_valley; 
+            break;
+            case 5:
+            System.out.println("You have choosen FOREST FANTASY");
+            bill_amount = forest_fantasy;
+            break;
+            default:
+            System.out.println("Invalid input");
+            break;
+        }
+    
+        return bill_amount;
+    }
+
+
+    private int CalculateBill_Selective_Customization(int template_amount)
+    {
+        int bill_amount=template_amount;
         String choice = "Y"; 
         do
         {
@@ -89,7 +135,8 @@ class customised_model{
         }
         else if(choice == 2)
         {
-            bill_amount = CalculateBill_Selective_Customization();
+            int template_amount = existing_bouquests();
+            bill_amount = CalculateBill_Selective_Customization(template_amount);
         }
         return bill_amount;
     }
